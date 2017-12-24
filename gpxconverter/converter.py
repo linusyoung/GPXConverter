eb8167ffd6eb89381800a7fc03df6de1a14bde04
linusyoung <linusyoungrice@gmail.com>
2017-12-24 22:02:34 +1000

    fix help output.

--- gpxconverter/converter.py
+++ gpxconverter/converter.py
@@ -16,7 +16,8 @@ def convert(argv):
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'test.py -i <inputfile> -o <outputfile>'
+            print 'gpxconvert -i <inputfile> -o <outputfile>'
+            print 'for more help information. please visit github site.'
             sys.exit()
         elif opt in ('-i', '--ifile'):
             inputfile = arg
